989aa71aaa9aaa9aba744a5f541d5c26822d776f
Eswar-410 <avala.eswar.33.2@mountblue.tech>
2025-07-19 11:11:24 +0530

    update files

--- stack_over_flow/settings.py
+++ stack_over_flow/settings.py
@@ -77,16 +77,8 @@ WSGI_APPLICATION = "stack_over_flow.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
